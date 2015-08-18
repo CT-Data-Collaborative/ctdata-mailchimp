ef6e43cce9d2f72fa8bc65c81ce4dff5e69ec629
Sasha Cuerda <sasha@mapconomy.com>
2015-08-18 18:35:18 +0200

    tweaking to use crispy forms

--- ctdata_mailchimp/forms.py
+++ ctdata_mailchimp/forms.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+from crispy_forms.bootstrap import FormActions
 
 
 class SubscriptionPluginForm(forms.Form):
@@ -9,6 +12,14 @@ class SubscriptionPluginForm(forms.Form):
     last_name = forms.CharField(label=_('Last Name'))
     plugin_id = forms.CharField(widget=forms.HiddenInput)
     redirect_url = forms.CharField(widget=forms.HiddenInput)
+
+    def __init__(self, *args, **kwargs):
+        super(SubscriptionPluginForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_class = 'mailchimp-subscription-form'
+        self.helper.add_input(Submit('submit', 'Submit')) 
+        self.helper.form_action = "{% url 'admin:ctdata-mailchimp-subscribe' %}"
 
 class UnsubscribePluginForm(forms.Form):
     email = forms.EmailField(max_length=100, label=_('E-mail'))
