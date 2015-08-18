06f178d0399084df37de086b003845869c737047
Sasha Cuerda <sasha@mapconomy.com>
2015-08-18 14:52:38 +0200

    disable app

--- ctdata_mailchimp/cms_app.py
+++ ctdata_mailchimp/cms_app.py
@@ -5,9 +5,9 @@ from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 
 
-class MailchimpUserMGMT(CMSApp):
-    name = _('Mailchimp User Management')
-    urls = ['ctdata_mailchimp.urls']
+# class MailchimpUserMGMT(CMSApp):
+#     name = _('Mailchimp User Management')
+#     urls = ['ctdata_mailchimp.urls']
 
 
-apphook_pool.register(MailchimpUserMGMT)
+# apphook_pool.register(MailchimpUserMGMT)
